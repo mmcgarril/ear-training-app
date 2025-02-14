00e889fcd061b15f7265c0ef4d44a306b63065ce
Michael McGarril <michaelmcgarril@gmail.com>
2025-02-14 13:11:41 -0500

    Add header contents and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 export default function Header() {
     return (
         <>
-        
+            <div className="header">
+                <i class="fa-solid fa-house"></i>
+                <div className="score-div">
+                    <p>0/0</p>
+                    <p>100%</p>
+                </div>
+                <i class="fa-solid fa-gear"></i>
+            </div>
         </>
     )
 }
